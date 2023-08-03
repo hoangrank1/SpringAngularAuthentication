92f4e44c60e420203ccb38764a97959168ec9e88
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-08-03 07:58:43 +0700

    crudOperation set up

---
+++ crudOperation/server/src/test/java/com/angularspring/practiceone/PracticeoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularspring.practiceone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
