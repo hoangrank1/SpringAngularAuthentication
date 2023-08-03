867e038032c4f4c7ccd5f1ab7d87b8659ef5847b
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-08-03 07:57:30 +0700

    fileUpload set up

---
+++ fileUpload/server/src/main/java/com/angularspring/practicetwo/PracticetwoApplication.java
@@ -0,0 +1,13 @@
+package com.angularspring.practicetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticetwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticetwoApplication.class, args);
+	}
+
+}
---
+++ fileUpload/server/src/test/java/com/angularspring/practicetwo/PracticetwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularspring.practicetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticetwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
