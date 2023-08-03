f9c1a9b9c9a74723493a509ac96b789999ebdb75
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-08-03 10:53:57 +0700

    fileUpload client service

--- fileUpload/client/src/app/app.module.ts
+++ fileUpload/client/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
+import { AllFilesComponent } from './components/all-files/all-files.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFileComponent,
+    AllFilesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
